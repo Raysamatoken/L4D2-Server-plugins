f1cfe093068c3d91597e41b12e3f6e91bd4ad304
Raysamatoken <1012274122@qq.com>
2021-02-09 12:44:53 +0800

    update Sourcemod

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./srcds_run -game left4dead2 -secure +maxplayers 16 +map c2m1_highway -condebug +exec server.cfg -tickrate 100 -nomaster
+./srcds_run -game left4dead2 -secure +maxplayers 8 +map c2m1_highway -condebug +exec server.cfg -tickrate 100 -nomaster
 
